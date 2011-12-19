cbbe74b8bc4e310ecab2d58bdc4776bab2024074
Keisuke Tsushima <keisuketsushima@yahoo.co.jp>
2011-12-19 11:45:25 +0900

    create time.f90 & modify time.sh

--- time.sh
+++ time.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+outputfile=time.csv
+echo '\"Number of Extent\", RMO, CMO' >> $outputfile
 for i in {1..250}
 do n=$(($i*2));
-    echo $n | ./a.out >> time_column.csv
+    echo $n | ./a.out >> $outputfile
     echo End $n
 done
