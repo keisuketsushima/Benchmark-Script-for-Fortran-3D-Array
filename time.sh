c4542f96f6feae46031d78d0a74a7a3f49cbcc2e
Keisuke Tsushima <keisuketsushima@yahoo.co.jp>
2011-12-19 20:17:38 +0900

    automate operation

--- time.sh
+++ time.sh
@@ -1,9 +1,24 @@
 #!/bin/bash
 
-outputfile=time.csv
+case "$0" in
+/*) A=`dirname "$0"` ;;
+*) A=`dirname "$PWD/$0"` ;;
+esac
+echo $A
+
+#change to your own fortran compiler
+gfortran -o $A/a.out $A/script/time.f90
+
+outputfile=$A/result/time-`date +%Y%m%d%H%M%S`.csv
+startTime=`date +%s`
 echo '"Number of Extent", RMO, CMO' >> $outputfile
-for i in {1..250}
+for i in {1..50}
 do n=$(($i*2));
-    echo $n | ./a.out >> $outputfile
+    echo $n | $A/a.out >> $outputfile
     echo End $n
 done
+endTime=`date +%s`
+
+rm $A/a.out
+
+echo duration , $startTime, $endTime >> $outputfile
