666fac10180993eccc2072b9e686368d92314cd7
Keisuke Tsushima <keisuketsushima@yahoo.co.jp>
2011-12-19 19:40:46 +0900

    cleanup directory

--- time.sh
+++ time.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 outputfile=time.csv
-echo '\"Number of Extent\", RMO, CMO' >> $outputfile
+echo '"Number of Extent", RMO, CMO' >> $outputfile
 for i in {1..250}
 do n=$(($i*2));
     echo $n | ./a.out >> $outputfile
